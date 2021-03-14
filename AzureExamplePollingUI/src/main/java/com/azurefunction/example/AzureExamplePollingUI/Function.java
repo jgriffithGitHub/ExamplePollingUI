c13202d90698f46aa2d5a2713e0fcbeae4aa0361
Example PolligCorp <examplePollingCorp@gmail.com>
2021-03-14 12:51:23 -0700

    Changed function name

--- AzureExamplePollingUI/src/main/java/com/azurefunction/example/AzureExamplePollingUI/Function.java
+++ AzureExamplePollingUI/src/main/java/com/azurefunction/example/AzureExamplePollingUI/Function.java
@@ -9,35 +9,62 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.logging.Logger;
+import java.util.Map.Entry;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
-public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
-     */
-    @FunctionName("HttpExample")
-    public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
+public class Function
+{
+	/**
+	 * This function listens at endpoint "/api/HttpExample". Two ways to invoke it
+	 * using "curl" command in bash: 1. curl -d "HTTP Body" {your
+	 * host}/api/HttpExample 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+	 */
+	@FunctionName("GetVotingUI")
+	public HttpResponseMessage run(@HttpTrigger(name = "req", methods =
+	{ HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+			final ExecutionContext context)
+	{
+		Logger log = context.getLogger();
+		
+		log.info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+		log = context.getLogger();
+		log.info("Java HTTP trigger processed a request.");
+		log.info("Request Method: " + request.getHttpMethod());
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
-        }
-    }
+		String principalName = "None provided";
+		String principalId = "None provided";
+		
+		String retText = "Headers: ";
+		Map<String,String> headers = request.getHeaders();
+		
+		if(headers == null || headers.isEmpty())
+		{
+			log.info("No headers");			
+		}
+		else
+		{
+			Set<Entry<String, String>> keySet = headers.entrySet();
+			if(keySet == null || keySet.isEmpty())
+			{
+				log.info("No headers");			
+			}
+			else
+			{				
+				principalName = headers.get("x-ms-client-principal-name");
+				principalId = headers.get("x-ms-client-principal-id");
+			}
+		}
+		
+		log.info("principalName: " + principalName);
+		log.info("principalId: " + principalId);
+		
+		return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + principalName).build();
+	}
 }
